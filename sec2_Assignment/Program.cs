b0b2a05441b439eafbc656b4c034f036108acf7d
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-20 17:05:24 +0300

    Q1 Create a Book class

--- sec2_Assignment/Program.cs
+++ sec2_Assignment/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 Create a Book class 
+            //book b1 = new book();
+            //b1.title = "100 ways to cook potatos";
+            //b1.pages = 100;
+            //object b1Obj = b1;
+            //Console.WriteLine(b1Obj.ToString());
+            #endregion
+
         }
     }
 }
---
+++ sec2_Assignment/book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sec2_Assignment
+{
+    internal class book
+    {
+       public string title;
+       public int pages;
+        public override string ToString()
+        {
+            return $"Title: {title}, Pages: {pages}";
+        }
+    }
+    
+}
