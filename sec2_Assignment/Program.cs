416f521d229875aa51c0a85dddfbf2d00c581239
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-20 17:11:55 +0300

    Q1 another answer Create a Book class

--- sec2_Assignment/Program.cs
+++ sec2_Assignment/Program.cs
@@ -11,7 +11,15 @@
             //object b1Obj = b1;
             //Console.WriteLine(b1Obj.ToString());
             #endregion
-
+            #region Q1 another answer Create a Book class 
+            //book b1 = new book();
+            //b1.title = "100 ways to cook potatos";
+            //b1.pages = 100;
+            //object b1Obj = b1;
+            //Console.WriteLine($"title is :{b1.title} , number of padges = {b1.pages}");
+            #endregion
+            #region Q2
+            #endregion
         }
     }
 }
