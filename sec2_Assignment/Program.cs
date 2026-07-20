3a2f20ff9e30eaa586174ef8157a6e269a0071ff
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-20 17:39:10 +0300

    Q2 print the result of calling ToString(), Equals()(compare book with itself), GetHashCode(), and GetType() on book.

--- sec2_Assignment/Program.cs
+++ sec2_Assignment/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace sec2_Assignment
+﻿using System.Net.NetworkInformation;
+
+namespace sec2_Assignment
 {
     internal class Program
     {
@@ -18,7 +20,14 @@
             //object b1Obj = b1;
             //Console.WriteLine($"title is :{b1.title} , number of padges = {b1.pages}");
             #endregion
-            #region Q2
+            #region Q2 print the result of calling ToString(), Equals()(compare book with itself), GetHashCode(), and GetType() on book.
+            //book b1 = new book();
+            //b1.title = "100 ways to cook potatos";
+            //b1.pages = 100;
+            //Console.WriteLine(b1.ToString());
+            //Console.WriteLine(b1.Equals(b1));
+            //Console.WriteLine(b1.GetHashCode());
+            //Console.WriteLine(b1.GetType());
             #endregion
         }
     }
